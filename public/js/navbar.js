8af98bfe2bb89274813eae478e6018e351039a30
Zumi <mizuzumi16@gmail.com>
2025-10-08 18:45:23 +0800

    Fixed navigation menu for smaller screen

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,6 +1,7 @@
 const navbar = document.getElementById('navbar');
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const search = document.querySelector(".search");
 
 function handleScroll() {
   if (window.scrollY > 10) {
@@ -17,4 +18,6 @@ window.addEventListener('scroll', handleScroll);
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
+  navbar.classList.toggle("active");
+  search.style.visibility("invisible");
 })
